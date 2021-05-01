7205e86d79967a38f468f42e9df3b202dc421cd4
thecosmiclegend <83489757+thecosmiclegend@users.noreply.github.com>
2021-05-01 13:05:37 +0530

    Add files via upload

---
+++ app/src/main/java/com/movister/cl/MainActivity.java
@@ -0,0 +1,285 @@
+package com.movister.cl;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.graphics.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.text.*;
+import android.widget.Button;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.AdRequest;
+import android.view.View;
+import android.webkit.WebViewClient;
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+
+public class MainActivity extends AppCompatActivity {
+	
+	
+	private Button button1;
+	private WebView webview1;
+	private AdView adview1;
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		com.google.firebase.FirebaseApp.initializeApp(this);
+		initialize(_savedInstanceState);
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+		|| ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+			ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+		}
+		else {
+			initializeLogic();
+		}
+	}
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		if (requestCode == 1000) {
+			initializeLogic();
+		}
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		button1 = (Button) findViewById(R.id.button1);
+		webview1 = (WebView) findViewById(R.id.webview1);
+		webview1.getSettings().setJavaScriptEnabled(true);
+		webview1.getSettings().setSupportZoom(true);
+		adview1 = (AdView) findViewById(R.id.adview1);
+		
+		button1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				webview1.loadUrl("https://massive-enemies.000webhostapp.com");
+			}
+		});
+		
+		webview1.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView _param1, String _param2, Bitmap _param3) {
+				final String _url = _param2;
+				
+				super.onPageStarted(_param1, _param2, _param3);
+			}
+			
+			@Override
+			public void onPageFinished(WebView _param1, String _param2) {
+				final String _url = _param2;
+				
+				super.onPageFinished(_param1, _param2);
+			}
+		});
+	}
+	private void initializeLogic() {
+		_download_path("/Webview/Download/");
+		webview1.setWebChromeClient(new CustomWebClient());
+		webview1.setDownloadListener(new DownloadListener() {
+			public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+				
+				
+				DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+				String cookies = CookieManager.getInstance().getCookie(url);
+				request.addRequestHeader("cookie", cookies);
+				request.addRequestHeader("User-Agent", userAgent);
+				request.setDescription("Downloading file...");
+				request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+				request.allowScanningByMediaScanner(); request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+				
+				
+				java.io.File aatv = new java.io.File(Environment.getExternalStorageDirectory().getPath() + "/Webview/Download");if(!aatv.exists()){if (!aatv.mkdirs()){ Log.e("TravellerLog ::","Problem creating Image folder");}} request.setDestinationInExternalPublicDir("/Webview/Download", URLUtil.guessFileName(url, contentDisposition, mimetype));
+				
+				DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+				manager.enqueue(request);
+				showMessage("Downloading File....");
+				BroadcastReceiver onComplete = new BroadcastReceiver() {
+					public void onReceive(Context ctxt, Intent intent) {
+						showMessage("Download Complete!");
+						unregisterReceiver(this);
+					}};
+				registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+			}
+		});
+		webview1.loadUrl("https://undefinedmovies.blogspot.com");
+		webview1.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		webview1.goBack();
+	}
+	
+	@Override
+	protected void onPostCreate(Bundle _savedInstanceState) {
+		super.onPostCreate(_savedInstanceState);
+		
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		
+	}
+	
+	@Override
+	public void onStop() {
+		super.onStop();
+		
+	}
+	private void _download_path (final String _file_path) {
+		if (true) {
+			
+		}
+		else {
+			
+		}
+	}
+	
+	
+	private void _extra () {
+	}
+	
+	public class CustomWebClient extends WebChromeClient {
+		private View mCustomView;
+		private WebChromeClient.CustomViewCallback mCustomViewCallback;
+		protected FrameLayout frame;
+		
+		// Initially mOriginalOrientation is set to Landscape
+		private int mOriginalOrientation = android.content.pm.ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+		private int mOriginalSystemUiVisibility;
+		
+		// Constructor for CustomWebClient
+		public CustomWebClient() {}
+		
+		public Bitmap getDefaultVideoPoster() {
+			if (MainActivity.this == null) {
+				return null; }
+			return BitmapFactory.decodeResource(MainActivity.this.getApplicationContext().getResources(), 2130837573); }
+		
+		public void onShowCustomView(View paramView, WebChromeClient.CustomViewCallback viewCallback) {
+			if (this.mCustomView != null) {
+				onHideCustomView();
+				return; }
+			this.mCustomView = paramView;
+			this.mOriginalSystemUiVisibility = MainActivity.this.getWindow().getDecorView().getSystemUiVisibility();
+			// When CustomView is shown screen orientation changes to mOriginalOrientation (Landscape).
+			MainActivity.this.setRequestedOrientation(this.mOriginalOrientation);
+			// After that mOriginalOrientation is set to portrait.
+			this.mOriginalOrientation = android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+			this.mCustomViewCallback = viewCallback; ((FrameLayout)MainActivity.this.getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1)); MainActivity.this.getWindow().getDecorView().setSystemUiVisibility(3846);
+		}
+		
+		public void onHideCustomView() {
+			((FrameLayout)MainActivity.this.getWindow().getDecorView()).removeView(this.mCustomView);
+			this.mCustomView = null;
+			MainActivity.this.getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
+			// When CustomView is hidden, screen orientation is set to mOriginalOrientation (portrait).
+			MainActivity.this.setRequestedOrientation(this.mOriginalOrientation);
+			// After that mOriginalOrientation is set to landscape.
+			this.mOriginalOrientation = android.content.pm.ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE; this.mCustomViewCallback.onCustomViewHidden();
+			this.mCustomViewCallback = null;
+		}
+	}
+	
+	{
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
